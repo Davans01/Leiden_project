c64bdb86144e724fa366e991f635b8774131d5a6
jajyy <1040404@mborijnland.nl>
2021-03-09 16:30:10 +0100

    Load login info on load

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from "vue"
 import App from "./app.vue"
+import { request } from "./request"
 import router from "./router"
 import { store } from "./store"
 
 export const app = createApp(App)
   .use(router)
   .use(store)
-  .mount("#app")
+
+request("GET /auth/me").then(response => {
+  store.commit("setAuthUserData", response.ok ? response.data : null)
+  app.mount("#app")
+})
