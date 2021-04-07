62b8fe26bb913469354a10a99093edcc312351df
jajyy <1040404@mborijnland.nl>
2021-04-07 14:35:49 +0200

    Different domains for prod

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ import api.model.user
 from api.database import db, migrate
 from api.login import login_manager
 
-cors = CORS(origins="http://localhost:8080", supports_credentials=True)
-
 
 def create_app():
     app = Flask(__name__)
@@ -27,6 +25,15 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SQLALCHEMY_ECHO"] = app.config["ENV"] != "production"
+
+    cors = CORS(
+        origins=(
+            "https://ugleiden.test"
+            if app.config["ENV"] == "production"
+            else "http://localhost:8080"
+        ),
+        supports_credentials=True,
+    )
 
     cors.init_app(app)
     db.init_app(app)
