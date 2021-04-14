0bb555dfe28a9990967f608143b4ed4764e91b51
jajyy <1040404@mborijnland.nl>
2021-04-14 12:05:09 +0200

    write attempt

--- api/bp/ttn.py
+++ api/bp/ttn.py
@@ -1,3 +1,8 @@
+from api.database import db
+from api.model.device import Device
+from api.model.measure_type import MeasureType
+from api.model.measurement import Measurement
+from api.model.measurement_row import MeasurementRow
 from flask import Blueprint, abort, current_app, request
 
 blueprint = Blueprint("tnn", __name__)
@@ -9,4 +14,26 @@ def tnn():
         return abort(401)
 
     data = request.get_json()
-    print(data)
+
+    device = Device.query.get(data["hardware_serial"])
+    if not device:
+        return abort(500)
+
+    measurement = Measurement()
+    measurement.device = device
+    db.session.add(measurement)
+
+    for (k, v) in data["payload_fields"].items():
+        type = MeasureType.query.get(int(k))
+        if not type:
+            continue
+
+        row = MeasurementRow()
+        row.measurement = measurement
+        row.type = type
+        row.value = float(v)
+        db.session.add(row)
+
+    db.session.commit()
+
+    return "", 204
